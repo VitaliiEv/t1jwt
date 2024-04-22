9a79c3332bc4c9672958d5236e11919bf6340f6b
Vitalii Solomonov <solomonov.v@gmail.com>
2024-04-22 20:10:34 +0300

    init

---
+++ src/test/java/com/github/vitaliiev/t1jwt/T1jwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitaliiev.t1jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1jwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
